932c1417132afa848a9feef88a679fe1a44bbd8b
nii_okantey <nunoogideon19@gmail.com>
2023-01-31 15:41:08 +0000

    updated files and added bg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Content from './Content';
+import Navigation from './Navigation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Hey There</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, doloribus quam quisquam rerum eligendi corrupti debitis nam incidunt repudiandae iste accusamus maiores corporis asperiores quod? A quibusdam incidunt explicabo distinctio molestias velit deserunt temporibus beatae? Accusantium quibusdam itaque accusamus impedit? Totam optio amet alias magni ipsam corrupti, exercitationem quod nesciunt.</div>
+        <Navigation />
+        <Content />
       </header>
     </div>
   );
---
+++ src/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Navigation = () => {
+  return (
+    <header className='header'>
+      <h1>My weather app</h1>
+    </header>
+  )
+}
+
+export default Navigation
